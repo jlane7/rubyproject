b87a2d24f2552bec1de0cd18300c92c53c8df6cd
Jerome <ln_jrm@yahoo.com>
2015-04-30 14:33:13 -0500

    rest api funcitonality get request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,10 @@ class HomeController < ApplicationController
 	@answer = @result.split(" ")[4]	
 	puts "mao"	
 	#output = %x[ #{'sudo su cif'} ]
-	#output = %x[ #{'export PATH=$PATH:/opt/cif/bin'} ]
-	#output1 = %x[ #{'cif -d -M -q google.com'} ]
+	#output0 = %x[ #{'export PATH=$PATH:/opt/cif/bin'} ]
+	@output1 = %x[ #{'curl -k -i -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36" -H "Accept => application/json" "https://localhost/api?apikey=4e230209-078a-4120-817f-9c25822fee36&q=example.com"'} ]
 	#puts output
+	#puts output0
+	
   end
 end
